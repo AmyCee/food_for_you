7a5752905bea802f7b39d25bb3bea0a3dec180b7
markjordan <thurggex@gmail.com>
2021-11-09 10:11:55 +0100

    Created a boolean variable networkImage to specify if the information being received on FoodContainer creation is a NetworkImage or not.

--- lib/widget/food_container.dart
+++ lib/widget/food_container.dart
@@ -3,36 +3,36 @@ import 'package:flutter/material.dart';
 class FoodContainer extends StatelessWidget {
   String? name;
   String? picture;
-   FoodContainer({Key? key, this.name, this.picture}) : super(key: key);
+  bool networkImage;
+   FoodContainer({Key? key, this.name, this.picture, this.networkImage = false}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(top: 20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            height: 80,
-            width: 80,
-            decoration: BoxDecoration(
-              color: Colors.red,
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage(picture!)
-              )
-            ),
-          ),
-          Container(
-            height: 80,
-            width: 180,
-            decoration: BoxDecoration(
-                color: Colors.blue
-            ),
-            child: Text(name!),
-          ),
-
-        ],
+      child: Container(
+        margin: EdgeInsets.all(10),
+        height: 150,
+        width: 80,
+        child: Container(
+          padding: EdgeInsets.all(10),
+          child: Text(name!, style: TextStyle(color: Colors.white, backgroundColor: Colors.black26),),
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.red,
+          image: networkImage ?
+            DecorationImage(
+              colorFilter: ColorFilter.mode(Colors.black26, BlendMode.darken),
+              fit: BoxFit.cover,
+              image: NetworkImage(picture!)
+            )
+              :
+            DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage(picture!)
+          )
+        ),
       ),
     );
   }
