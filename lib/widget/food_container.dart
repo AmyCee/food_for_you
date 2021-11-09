2a6a3dfc27ae806f8a18eec49e2530a87c03fb2a
markjordan <thurggex@gmail.com>
2021-11-09 12:15:07 +0100

    Created a function to be called when an instantiation of the FoodContainer class in clicked (pressed). Added a MaterialButton to implement the onPressed functionality with its animation.

--- lib/widget/food_container.dart
+++ lib/widget/food_container.dart
@@ -4,34 +4,38 @@ class FoodContainer extends StatelessWidget {
   String? name;
   String? picture;
   bool networkImage;
-   FoodContainer({Key? key, this.name, this.picture, this.networkImage = false}) : super(key: key);
+  VoidCallback? widgetClick;
+
+  FoodContainer({Key? key, this.name, this.picture, this.networkImage = false, this.widgetClick}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(top: 20),
-      child: Container(
-        margin: EdgeInsets.all(10),
-        height: 150,
-        width: 80,
+      margin: EdgeInsets.all(10),
+      height: MediaQuery.of(context).size.height * 0.25,
+      width: MediaQuery.of(context).size.width * 0.8,
+      decoration: BoxDecoration(
+        color: Colors.red,
+        borderRadius: BorderRadius.circular(10),
+        image: networkImage ?
+          DecorationImage(
+            colorFilter: ColorFilter.mode(Colors.black26, BlendMode.darken),
+            fit: BoxFit.cover,
+            image: NetworkImage(picture!)
+          )
+            :
+          DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage(picture!)
+        )
+      ),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width * 0.8,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        onPressed: widgetClick!,
         child: Container(
           padding: EdgeInsets.all(10),
           child: Text(name!, style: TextStyle(color: Colors.white, backgroundColor: Colors.black26),),
-        ),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: Colors.red,
-          image: networkImage ?
-            DecorationImage(
-              colorFilter: ColorFilter.mode(Colors.black26, BlendMode.darken),
-              fit: BoxFit.cover,
-              image: NetworkImage(picture!)
-            )
-              :
-            DecorationImage(
-              fit: BoxFit.cover,
-              image: AssetImage(picture!)
-          )
         ),
       ),
     );
